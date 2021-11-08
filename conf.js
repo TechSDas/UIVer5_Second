aa5302fe5583ce96001268fc1b24644bb1ccc765
Subhajit <64370634+TechSDas@users.noreply.github.com>
2021-11-08 11:18:49 +0530

    Add files via upload

---
+++ conf.js
@@ -0,0 +1,12 @@
+exports.config = {
+    profile: 'integration',
+	baseUrl: 'https://steds-prod-iscoper-web-approuter.cfapps.sap.hana.ondemand.com/cp.portal/site#Shell-home',
+    // baseUrl: 'https://steds-prod-iscoper-web-approuter.cfapps.sap.hana.ondemand.com/cp.portal/site#testcase-maintain?sap-ui-app-id-hint=iscoper.maintaintestcase'
+	auth: {
+		'sapcloud-form': {
+			user: '',
+			pass: '',
+		}
+	},
+    specs: ['spec.js']
+  };
