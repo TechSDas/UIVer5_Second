b8aa0f609464ce9da9a3580f72920ffcabb73a84
Subhajit <64370634+TechSDas@users.noreply.github.com>
2021-11-10 13:28:25 +0530

    Update spec.js
    latest

--- spec.js
+++ spec.js
@@ -1,18 +1,30 @@
 describe('masterdetail', function () {
 
-    // it('should load the app',function() {
-    //   expect(browser.getTitle()).toBe('Master-Detail');
-    // });
+    it('should load the app',function() {
+      expect(browser.getTitle()).toBe('Maintain Test Cases for Automated TestPlans');
+    });
   
-    // it('should display the details screen',function() {
-    //   element(by.control({
-    //     viewName: 'sap.ui.demo.masterdetail.view.Master',
-    //     controlType: 'sap.m.ObjectListItem',
-    //     properties: {
-    //       title: 'Object 10'
-    //     }}))
-    //   .click();
+    it('should display the test case details screen',function() {
+      element(by.control({
+        viewName: 'iscoper/maintaintestcase/view/testPlanDetails.view',
+        controlType: 'sap.m.ColumnListItem',
+        properties: {
+          title: 'Object 10'
+        }}))
+      .click();
+    });
+
+
+    // it('should display the test case details screen',function() {
+    //   element.all(by.control(
+    //     {
+    //     viewName: 'iscoper/maintaintestcase/view/testPlanDetails.view',
+    //     controlType: 'sap.m.ColumnListItem',
+    //     id: "tab2"
+    //   }))
+    //   .first();
     // });
+
   
     // it('should validate line items',function() {
     //   expect(element.all(by.control({
